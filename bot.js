70035edb7ce356e6afbd3891c0593a10433a1df8
Henry Martinez <henrymtz0390@gmail.com>
2024-03-09 18:07:37 +0000

    Henry | Fix commit history of removing discord token in bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,22 @@
-import {Client} from "discord.js"
-const client = new Client();
-
+import { Client, Guild, GuildManager, GatewayIntentBits, User, GuildMemberManager, GuildMember, Collection } from "discord.js";
+const client = new Client({
+ intents: [
+   GatewayIntentBits.Guilds,
+   GatewayIntentBits.GuildMessages,
+   GatewayIntentBits.GuildMembers,
+ ],
+});
+import dotenv from "dotenv"
+dotenv.config() // Add only when developing, configure later for production
+const TOKEN = process.env.TOKEN;
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+ console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
-  }
+ if (msg.content === "ping") {
+   msg.reply("pong");
+ }
 })
 
-client.login()
+client.login(TOKEN)
